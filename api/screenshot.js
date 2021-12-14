495bfc486aa66054faf5f3da2c7f99e3fb257fa0
hahuytien <hahuytien513.cnpm@gmail.com>
2021-12-15 00:57:54 +0700

    add lib

--- api/screenshot.js
+++ api/screenshot.js
@@ -17,7 +17,7 @@ const option = {
 
 
 // Wrapping the Puppeteer browser logic in a GET request
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
     // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
     chromium.puppeteer.launch({
@@ -26,23 +26,27 @@ app.get('/', function(req, res) {
         executablePath: chromium.executablePath,
         headless: chromium.headless,
         ignoreHTTPSErrors: true,
-      }).then(async function(browser) {
+    }).then(async function (browser) {
         const page = await browser.newPage();
         await page.goto('http://digidb.io/digimon-list/');
 
         // Targeting the DOM Nodes that contain the Digimon names
-        const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function(digimons) {
-        // Mapping each Digimon name to an array
-            return digimons.map(function(digimon) {
-          return digimon.innerText;
+        const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function (digimons) {
+            // Mapping each Digimon name to an array
+            return digimons.map(function (digimon) {
+                return digimon.innerText;
+            });
         });
-      });
 
         // Closing the Puppeteer controlled headless browser
         await browser.close();
 
         // Sending the Digimon names to Postman
-        res.send(digimonNames);
+        // res.send(digimonNames);
+        res.json({
+            status: 200,
+            message: "Get data has successfully",
+        });
     });
 });
 
