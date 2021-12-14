43db12db54cab20b4ac00d7938c239c88baa3084
hahuytien <hahuytien513.cnpm@gmail.com>
2021-12-15 00:00:29 +0700

    add library

--- api/screenshot.js
+++ api/screenshot.js
@@ -5,33 +5,34 @@ const chromium = require('chrome-aws-lambda');
 
 
 // Wrapping the Puppeteer browser logic in a GET request
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
-    // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
-    chromium.puppeteer.launch({
-        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
-      }).then(async function(browser) {
-        const page = await browser.newPage();
-        await page.goto('http://digidb.io/digimon-list/');
-
-        // Targeting the DOM Nodes that contain the Digimon names
-        const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function(digimons) {
-        // Mapping each Digimon name to an array
-            return digimons.map(function(digimon) {
-          return digimon.innerText;
+    try {
+        browser = await chromium.puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
         });
-      });
 
-        // Closing the Puppeteer controlled headless browser
-        await browser.close();
+        let page = await browser.newPage();
 
-        // Sending the Digimon names to Postman
-        res.send(digimonNames);
-    });
+        await page.goto(event.url || 'https://example.com');
+
+        result = await page.title();
+    } catch (error) {
+        return callback(error);
+    } finally {
+        if (browser !== null) {
+            await browser.close();
+        }
+        res.json({
+            status: 200,
+            message: "Get data has successfully",
+        });
+    }
+
 });
 
 
