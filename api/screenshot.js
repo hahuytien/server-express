2d19873d5fb277e0c220caeae709d054c617ecb6
hahuytien <hahuytien513.cnpm@gmail.com>
2021-12-14 22:38:29 +0700

    add api

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,55 +1,30 @@
-const express = require("express");
-const router = express.Router();
-const puppeteer = require('puppeteer');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const delay = require('delay');
-const axios = require('axios');
-const { convert } = require('html-to-text');
+const express = require('express'); // Adding Express
+const app = express(); // Initializing Express
+const puppeteer = require('puppeteer'); // Adding Puppeteer
 
-/**
- * GET product list.
- *
- * @return product list | empty.
- */
-router.get("/", async (req, res) => {
-//   try {
-    const browser = await puppeteer.launch(
-        {
-            headless: true,
-            args: ['--no-sandbox']
-        }
-    );
-    const context = browser.defaultBrowserContext();
-    context.overridePermissions("https://www.facebook.com", ["geolocation", "notifications"]);
+// Wrapping the Puppeteer browser logic in a GET request
+app.get('/', function(req, res) {
 
-    await delay(2000);
-    const page = await browser.newPage();
-    await page.goto(req.query.url, { waitUntil: 'networkidle0' });
-    await page.setExtraHTTPHeaders({
-        'Accept-Language': 'es-US'
-    })
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36');
-    await page.setJavaScriptEnabled(true);
+    // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+    puppeteer.launch().then(async function(browser) {
+        const page = await browser.newPage();
+        await page.goto('http://digidb.io/digimon-list/');
 
-    // const screenshotBuffer = await page.screenshot();
-
-    // // Respond with the image
-    // res.writeHead(200, {
-    //     'Content-Type': 'image/png',
-    //     'Content-Length': screenshotBuffer.length
-    // });
-    // res.end(screenshotBuffer);
-
-    // await delay(1000);
-    // await browser.close();
-    res.json({
-        status: 200,
-        message: "Get data has successfully",
+        // Targeting the DOM Nodes that contain the Digimon names
+        const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function(digimons) {
+        // Mapping each Digimon name to an array
+            return digimons.map(function(digimon) {
+          return digimon.innerText;
+        });
       });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).send("Server error23123123");
-//   }
+
+        // Closing the Puppeteer controlled headless browser
+        await browser.close();
+
+        // Sending the Digimon names to Postman
+        res.send(digimonNames);
+    });
 });
 
-module.exports = router;
+
+module.exports = app;
