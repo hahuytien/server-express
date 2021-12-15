7ffdf62bb4a8c0422d5d09e5b059a79c149d5fca
Tienhh <tienhh@antsoft.vn>
2021-12-15 13:58:46 +0700

    add lib

--- api/screenshot.js
+++ api/screenshot.js
@@ -3,6 +3,9 @@ const app = express(); // Initializing Express
 // const puppeteer = require('puppeteer'); // Adding Puppeteer
 const chromium = require('chrome-aws-lambda');
 const PCR = require("puppeteer-chromium-resolver");
+const delay = require('delay');
+const axios = require('axios');
+const { convert } = require('html-to-text');
 
 const option = {
     revision: "",
@@ -28,10 +31,20 @@ app.get('/', function (req, res) {
             headless: chromium.headless,
             ignoreHTTPSErrors: true
         }).then(async function (browser) {
+
+            const context = browser.defaultBrowserContext();
+            context.overridePermissions("https://www.facebook.com", ["geolocation", "notifications"]);
+
+            await delay(2000);
             const page = await browser.newPage();
-            await page.goto('https://www.facebook.com/');
+            await page.goto('https://www.facebook.com/', { waitUntil: 'networkidle0' });
 
+            await page.setExtraHTTPHeaders({
+                'Accept-Language': 'es-US'
+            })
 
+            await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36');
+            await page.setJavaScriptEnabled(true);
 
             const screenshotBuffer = await page.screenshot();
 
