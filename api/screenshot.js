ca1ce5038246b6ba022e5d101cf64e57c9bca2d6
Tienhh <tienhh@antsoft.vn>
2021-12-15 13:14:37 +0700

    fix api

--- api/screenshot.js
+++ api/screenshot.js
@@ -29,27 +29,29 @@ app.get('/', function (req, res) {
             ignoreHTTPSErrors: true
         }).then(async function (browser) {
             const page = await browser.newPage();
-            await page.goto('http://digidb.io/digimon-list/');
+            await page.goto('https://example.com/');
 
             // Targeting the DOM Nodes that contain the Digimon names
-            const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function (digimons) {
-                // Mapping each Digimon name to an array
-                return digimons.map(function (digimon) {
-                    return digimon.innerText;
-                });
-            });
+            // const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function (digimons) {
+            //     // Mapping each Digimon name to an array
+            //     return digimons.map(function (digimon) {
+            //         return digimon.innerText;
+            //     });
+            // });
 
             // Closing the Puppeteer controlled headless browser
             await browser.close();
 
             // Sending the Digimon names to Postman
             // res.send(digimonNames);
+        
+            await page.close()
+
             res.json({
                 status: 200,
                 message: "Get data has successfully",
             });
 
-            await page.close()
         });
     })()
     .catch(err => res.sendStatus(500));
