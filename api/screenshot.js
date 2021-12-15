65cda1b5d8296b67638d4cce0b5a9b7cd9cb68ad
Tienhh <tienhh@antsoft.vn>
2021-12-15 11:56:00 +0700

    fix api

--- api/screenshot.js
+++ api/screenshot.js
@@ -19,52 +19,36 @@ const option = {
 // Wrapping the Puppeteer browser logic in a GET request
 app.get('/', function (req, res) {
 
-    // (async () => {
-    //     // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
-    //     chromium.puppeteer.launch({
-    //         args: chromium.args,
-    //         defaultViewport: chromium.defaultViewport,
-    //         executablePath: await chromium.executablePath,
-    //         headless: chromium.headless,
-    //         ignoreHTTPSErrors: true
-    //     }).then(async function (browser) {
-    //         const page = await browser.newPage();
-    //         await page.goto('http://digidb.io/digimon-list/');
-
-    //         // Targeting the DOM Nodes that contain the Digimon names
-    //         const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function (digimons) {
-    //             // Mapping each Digimon name to an array
-    //             return digimons.map(function (digimon) {
-    //                 return digimon.innerText;
-    //             });
-    //         });
-
-    //         // Closing the Puppeteer controlled headless browser
-    //         await browser.close();
-
-    //         // Sending the Digimon names to Postman
-    //         // res.send(digimonNames);
-    //         res.json({
-    //             status: 200,
-    //             message: "Get data has successfully",
-    //         });
-    //     });
-    // })()
-    // .catch(err => res.sendStatus(500))
-    // .finally(async () => await page.close());
     (async () => {
-        browser = await chromium.puppeteer.launch({
+        // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+        chromium.puppeteer.launch({
             args: chromium.args,
             defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath,
             headless: chromium.headless,
-            ignoreHTTPSErrors: true,
+            ignoreHTTPSErrors: true
+        }).then(async function (browser) {
+            const page = await browser.newPage();
+            await page.goto('http://digidb.io/digimon-list/');
+
+            // Targeting the DOM Nodes that contain the Digimon names
+            const digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function (digimons) {
+                // Mapping each Digimon name to an array
+                return digimons.map(function (digimon) {
+                    return digimon.innerText;
+                });
+            });
+
+            // Closing the Puppeteer controlled headless browser
+            await browser.close();
+
+            // Sending the Digimon names to Postman
+            // res.send(digimonNames);
+            res.json({
+                status: 200,
+                message: "Get data has successfully",
+            });
         });
-
-        let page = await browser.newPage();
-        await page.goto('https://example.com');
-
-        res.send(await page.content());
     })()
     .catch(err => res.sendStatus(500))
     .finally(async () => await page.close());
