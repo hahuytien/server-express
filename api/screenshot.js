43a3595a4adebcc0aee6081986bd402effc3b92e
Tienhh <tienhh@antsoft.vn>
2021-12-15 13:40:47 +0700

    fix api

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,77 +1,87 @@
-// const express = require('express'); // Adding Express
-// const app = express(); // Initializing Express
-// // const puppeteer = require('puppeteer'); // Adding Puppeteer
-// const chromium = require('chrome-aws-lambda');
-// const PCR = require("puppeteer-chromium-resolver");
+const express = require('express'); // Adding Express
+const app = express(); // Initializing Express
+// const puppeteer = require('puppeteer'); // Adding Puppeteer
+const chromium = require('chrome-aws-lambda');
+const PCR = require("puppeteer-chromium-resolver");
 
-// const option = {
-//     revision: "",
-//     detectionPath: "",
-//     folderName: ".chromium-browser-snapshots",
-//     defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
-//     hosts: [],
-//     cacheRevisions: 2,
-//     retry: 3,
-//     silent: false
-// };
-
-
-// // Wrapping the Puppeteer browser logic in a GET request
-// app.get('/', function (req, res) {
-
-//     (async () => {
-//         // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
-//         chromium.puppeteer.launch({
-//             args: chromium.args,
-//             defaultViewport: chromium.defaultViewport,
-//             executablePath: await chromium.executablePath,
-//             headless: chromium.headless,
-//             ignoreHTTPSErrors: true
-//         }).then(async function (browser) {
-//             const page = await browser.newPage();
-//             await page.goto('https://example.com/');
+const option = {
+    revision: "",
+    detectionPath: "",
+    folderName: ".chromium-browser-snapshots",
+    defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
+    hosts: [],
+    cacheRevisions: 2,
+    retry: 3,
+    silent: false
+};
 
 
-       
-//             const screenshotBuffer = await page.screenshot();
+// Wrapping the Puppeteer browser logic in a GET request
+app.get('/', function (req, res) {
 
-//             // Respond with the image
-//             res.writeHead(200, {
-//                 'Content-Type': 'image/png',
-//                 'Content-Length': screenshotBuffer.length
-//             });
-//             res.end(screenshotBuffer);
-
-//             await browser.close();
-
-//         });
-//     })()
-//         .catch(err => res.sendStatus(500));
-// });
+    (async () => {
+        // // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+        // chromium.puppeteer.launch({
+        //     args: chromium.args,
+        //     defaultViewport: chromium.defaultViewport,
+        //     executablePath: await chromium.executablePath,
+        //     headless: chromium.headless,
+        //     ignoreHTTPSErrors: true
+        // }).then(async function (browser) {
+        //     const page = await browser.newPage();
+        //     await page.goto('https://example.com/');
 
 
-// module.exports = app;
+
+        //     const screenshotBuffer = await page.screenshot();
+
+        //     // Respond with the image
+        //     res.writeHead(200, {
+        //         'Content-Type': 'image/png',
+        //         'Content-Length': screenshotBuffer.length
+        //     });
+        //     res.end(screenshotBuffer);
+
+        //     await browser.close();
+
+        // });
 
 
-const express = require("express");
-const puppeteer = require("puppeteer");
 
-const app = express();
-const browserP = puppeteer.launch({
-  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+
+
+
+        const browser = await puppeteer.launch(
+            {
+                headless: true,
+                args: ["--no-sandbox", "--disable-setuid-sandbox"]
+            }
+        );
+        const context = browser.defaultBrowserContext();
+        context.overridePermissions("https://www.facebook.com", ["geolocation", "notifications"]);
+        const page = await browser.newPage();
+        await page.goto(req.query.url, { waitUntil: 'networkidle0' });
+        await page.setExtraHTTPHeaders({
+            'Accept-Language': 'es-US'
+        })
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36');
+        await page.setJavaScriptEnabled(true);
+
+
+
+        const screenshotBuffer = await page.screenshot();
+
+        // Respond with the image
+        res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': screenshotBuffer.length
+        });
+        res.end(screenshotBuffer);
+
+        await browser.close();
+    })()
+        .catch(err => res.sendStatus(500));
 });
 
-app.get("/", (req, res) => {
-  // FIXME move to a worker task; see https://devcenter.heroku.com/articles/node-redis-workers
-  let page;
-  (async () => {
-    page = await (await browserP).newPage();
-    await page.setContent(`<p>web running at ${Date()}</p>`);
-    res.send(await page.content());
-  })()
-    .catch(err => res.sendStatus(500))
-    .finally(async () => await page.close())
-  ;
-});
 
 module.exports = app;
